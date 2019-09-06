1fc9c912d126cb343f5f18cdff7565e47dc12f5f
root <root@devops.lab.com>
2019-09-05 21:37:54 -0400

    mod

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http'); 
 http.createServer(function (req, res) { 
   res.writeHead(200, {'Content-Type': 'text/plain'}); 
-  res.end('Hello, World..How are you!\n'); 
+  res.end('Hello, World..How are you...Hope its great!\n'); 
 }).listen(8124, '0.0.0.0'); 
 console.log('Server running at http://127.0.0.1:8124'); 
--- simple.js
+++ simple.js
@@ -1,5 +1,5 @@
 var count = 0;
 exports.next = function() { return ++count; };
 exports.hello = function() {
-  return "Hello, world...How are you!";
+  return "Hello, world...How are you...Hope its great!";
 };
